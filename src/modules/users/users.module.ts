65da195cc9217aaae1718c14ff538e19f185cb68
Trung Nguyen <minhtrung2031997@gmail.com>
2023-03-05 17:53:40 +0700

    completely integrate the authorization (role) for user endpoints

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesGuard } from '../../Guards/roles.guard';
 import { User } from './user.entity';
 import { UsersRepository } from './user.repository';
 import { UsersController } from './users.controller';
@@ -8,7 +11,15 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [
+    JwtService,
+    UsersService,
+    UsersRepository,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
   exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
